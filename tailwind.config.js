d7f970b7144a64679fcbc0fe3ab02e9d5597ccdc
Azur1337 <82388136+Azur1337@users.noreply.github.com>
2023-05-23 16:54:04 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["C:\Users\azur9\OneDrive\Desktop\portfolio\index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+module.exports = {
+  darkMode: 'class',
+  // ...
+}
+
