dadf2618741fda1d9ee1ffa155a99413e59b1ca1
Azur1337 <82388136+Azur1337@users.noreply.github.com>
2023-05-25 20:46:48 +0200

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["C:\Users\azur9\OneDrive\Desktop\portfolio\index.html"],
   theme: {
+    container: {
+      center: true,
+    },
+    colors: {
+      femboypink:'#cdb4db',
+    },
     extend: {},
   },
   plugins: [],
 }
+
+
